b084a6296b579676220187e8963dcac873f589dd
WillCAI2020 <CRC681225@163.com>
2022-05-11 16:44:20 +0800

    使用 Process.env

--- src/components/LibraryMenuItems.tsx
+++ src/components/LibraryMenuItems.tsx
@@ -335,7 +335,7 @@ const LibraryMenuItems = ({
           <Spinner />
         ) : (
           <a
-            href={`https://libraries.excalidraw.com/?target=${
+            href={`${process.env.REACT_APP_LIBRARY_URL}?target=${
               window.name || "_blank"
             }&referrer=${referrer}&useHash=true&token=${id}&theme=${theme}&version=${
               VERSIONS.excalidrawLibrary
